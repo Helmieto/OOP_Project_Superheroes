3925c6a75212c7a316fe07f2b7c5a7263d8ae9c5
Милен Темелков <migete@abv.bg>
2023-06-12 17:55:43 +0300

    Add User base class

---
+++ User.cpp
@@ -0,0 +1,43 @@
+#include "User.h"
+
+
+
+void User::setFirstName(const String& otherName) {
+	firstName = otherName;
+}
+
+void User::setLastName(const String& otherName) {
+	lastName = otherName;
+}
+
+void User::setUsername(const String& otherName) {
+	username = otherName;
+}
+
+void User::setEmail(const String& otherEmail) {
+	email = otherEmail;
+}
+
+void User::setPassword(const String& otherPass) {
+	password = otherPass;
+}
+
+String User::getFirstName() const {
+	return firstName;
+}
+
+String User::getLastName() const {
+	return lastName;
+}
+
+String User::getUsername() const {
+	return username;
+}
+
+String User::getEmail() const {
+	return email;
+}
+
+bool User::checkPassword(String otherPass) const {
+	return (password == otherPass);
+}
---
+++ User.h
@@ -0,0 +1,39 @@
+#pragma once
+#include"String.h"
+#include"Vector.hpp"
+#include"Superhero.h"
+
+enum class Type {
+	player, admin
+};
+
+class User
+{
+	String firstName;
+	String lastName;
+	String username;
+	String email;
+	String password;
+	
+public:
+
+	void setFirstName(const String& otherName);
+	void setLastName(const String& otherName);
+	void setUsername(const String& otherName);
+	void setEmail(const String& otherEmail);
+	void setPassword(const String& otherPass);
+
+	String getFirstName() const;
+	String getLastName() const;
+	String getUsername() const;
+	String getEmail() const;
+
+	bool checkPassword(String otherPass) const;
+
+	virtual Type identify() const = 0;
+	virtual void printInfoForPlayers() const = 0;
+	virtual void printInfoForAdmins() const = 0;
+
+	virtual ~User() = default;
+};
+
